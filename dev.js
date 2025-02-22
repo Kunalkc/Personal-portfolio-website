91579e23faab0b822a15fb75e288d55698ff3712
Kunal Chandel <Kunalchandel09@gmail.com>
2025-02-22 23:58:14 +0530

    setup react app and corresponding dependencies

---
+++ dev.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function App(){
+    return(
+        <p>hola welcome to the dev portfolio page</p>
+    )
+}
+
+root.render(<App/>)
