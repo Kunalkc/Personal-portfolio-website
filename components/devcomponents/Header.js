8a90f2308e8b40d49797a650484fea867dfbe670
Kunal Chandel <Kunalchandel09@gmail.com>
2025-02-23 20:30:40 +0530

    more awesome changes

--- components/devcomponents/Header.js
+++ components/devcomponents/Header.js
@@ -2,6 +2,9 @@ import React from "react";
 
 export default function header(){
   return(
-    <div className="head-bar"></div>
+    <div className="head-bar">
+     <div className="dev-header"><p>Dev Portfolio</p> </div>
+     <div className="Name-header"><p>KUNAL CHANDEL</p> </div>
+    </div>
   )
 }
